c2c17ce84ea7db0c058a55d680c97ccf5b213ec8
ThomasF85 <thomas.foeldi@gmail.com>
2023-03-11 21:26:07 +0100

    refactor

--- src/group/index.ts
+++ src/group/index.ts
@@ -1,10 +1,10 @@
 type Property = string | ((item: any) => string | number | boolean);
 
-function add(properties: Property[]) {
-  const property = properties[properties.length - 1];
-  let add;
+function addOnLeaf(
+  property: string | ((item: any) => string | number | boolean)
+): (data: { [value: string]: any }, item: any) => boolean {
   if (typeof property === "function") {
-    add = (data: any, item: any) => {
+    return (data: any, item: any) => {
       const value: any = property(item);
       if (value === null || value === undefined) {
         return false;
@@ -17,7 +17,7 @@ function add(properties: Property[]) {
       return true;
     };
   } else if (property.indexOf(".") === -1) {
-    add = (data: any, item: any) => {
+    return (data: any, item: any) => {
       const value: any = item[property];
       if (value === null || value === undefined) {
         return false;
@@ -31,7 +31,7 @@ function add(properties: Property[]) {
     };
   } else {
     const path = property.split(".");
-    add = (data: any, item: any) => {
+    return (data: any, item: any) => {
       let value = item;
       for (const property of path) {
         value = value[property];
@@ -47,63 +47,73 @@ function add(properties: Property[]) {
       return true;
     };
   }
-  let result = add;
+}
+
+function addOnNode(
+  property: string | ((item: any) => string | number | boolean),
+  addOnNextNode: (data: { [value: string]: any }, item: any) => boolean
+): (data: { [value: string]: any }, item: any) => boolean {
+  if (typeof property === "function") {
+    return (data: any, item: any) => {
+      const value: any = property(item);
+      if (value === null || value === undefined) {
+        return false;
+      }
+      if (data.hasOwnProperty(value)) {
+        return addOnNextNode(data[value], item);
+      }
+      const newData = {};
+      if (addOnNextNode(newData, item)) {
+        data[value] = newData;
+        return true;
+      }
+      return false;
+    };
+  } else if (property.indexOf(".") === -1) {
+    return (data: any, item: any) => {
+      const value: any = item[property];
+      if (value === null || value === undefined) {
+        return false;
+      }
+      if (data.hasOwnProperty(value)) {
+        return addOnNextNode(data[value], item);
+      }
+      const newData = {};
+      if (addOnNextNode(newData, item)) {
+        data[value] = newData;
+        return true;
+      }
+      return false;
+    };
+  } else {
+    const path = property.split(".");
+    return (data: any, item: any) => {
+      let value = item;
+      for (const property of path) {
+        value = value[property];
+        if (value === undefined || value === null) {
+          return false;
+        }
+      }
+      if (data.hasOwnProperty(value)) {
+        return addOnNextNode(data[value], item);
+      }
+      const newData = {};
+      if (addOnNextNode(newData, item)) {
+        data[value] = newData;
+        return true;
+      }
+      return false;
+    };
+  }
+}
+
+function add(properties: Property[]) {
+  const property = properties[properties.length - 1];
+  let result: (data: { [value: string]: any }, item: any) => boolean =
+    addOnLeaf(property);
   for (let i = properties.length - 2; i >= 0; i--) {
-    const prev = result;
-    const property = properties[i];
-    if (typeof property === "function") {
-      result = (data: any, item: any) => {
-        const value: any = property(item);
-        if (value === null || value === undefined) {
-          return false;
-        }
-        if (data.hasOwnProperty(value)) {
-          return prev(data[value], item);
-        }
-        const newData = {};
-        if (prev(newData, item)) {
-          data[value] = newData;
-          return true;
-        }
-        return false;
-      };
-    } else if (property.indexOf(".") === -1) {
-      result = (data: any, item: any) => {
-        const value: any = item[property];
-        if (value === null || value === undefined) {
-          return false;
-        }
-        if (data.hasOwnProperty(value)) {
-          return prev(data[value], item);
-        }
-        const newData = {};
-        if (prev(newData, item)) {
-          data[value] = newData;
-          return true;
-        }
-        return false;
-      };
-    } else {
-      const path = property.split(".");
-      result = (data: any, item: any) => {
-        let value = item;
-        for (const property of path) {
-          value = value[property];
-          if (value === undefined || value === null) {
-            return false;
-          }
-        }
-        if (data.hasOwnProperty(value)) {
-          return prev(data[value], item);
-        }
-        const newData = {};
-        if (prev(newData, item)) {
-          data[value] = newData;
-          return true;
-        }
-        return false;
-      };
-    }
+    result = addOnNode(properties[i], result);
   }
   return result;
 }
